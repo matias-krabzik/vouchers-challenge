3e68050b78fcf3f1c0c854ea2dfe9e5702084252
Matias Krabzik <krabzik@gmail.com>
2022-05-01 20:33:26 -0300

    Quitamos el Wellcome.

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,12 @@
 use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('login', [LoginController::class,'showLoginForm'])->name('login');
 Route::post('login', [LoginController::class,'login']);
 Route::post('logout', [LoginController::class,'logout'])->name('logout');
 
 Route::middleware(["auth"])->group(function () {
-    Route::get('/vouchers', [App\Http\Controllers\VoucherController::class, 'index'])->name('vouchers');
+    Route::get('/', [App\Http\Controllers\VoucherController::class, 'index'])->name('vouchers');
     Route::get('/export', [App\Http\Controllers\VoucherController::class, 'export'])->name('export');
 });
 
